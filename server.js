4492e120c5091e05871ee59374f85d45ad79dc5c
Matt Hernandez <matt@modulus.io>
2013-09-30 13:22:37 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express')
+  , joblint = require('joblint')
+  , app     = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.disable('x-powered-by');
+
+app.use(express.compress());
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/client'));
+app.use(app.router);
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.post('/', function(req, res) {
+  var result = joblint(req.body.spec);
+
+  res.render('result', {
+    result: result
+  });
+});
+
+app.listen(process.env.PORT || 3000);
